c953023aba2b863e5c08fc1bdf957a3debdd60ef
radek1251 <102186047+radek1251@users.noreply.github.com>
2022-04-04 16:17:16 +0200

    Update suma.js

--- suma/js/suma.js
+++ suma/js/suma.js
@@ -1,5 +1,9 @@
 
+
 let firstNumber = 10;
 let secondNumber = 5;
-let equal = firstNumber + secondNumber;
-console.log(equal); 
+function add(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+console.log(add(firstNumber,secondNumber))
